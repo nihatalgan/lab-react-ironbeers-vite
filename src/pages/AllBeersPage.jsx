d9ac69c2ad693c0187743d9d9016855bb4454daf
Nihat Algan <burakalgan@gmail.com>
2024-02-22 12:56:57 +0100

    done iteration 4

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,39 @@
-function AllBeersPage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function AllBeersPage() {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    // how to update the beers state with data
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers")
+      .then((response) => {
+        setBeers(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h3>List of Beers</h3>
+      {beers.map((beer) => {
+        return (
+          <div key={beers._id}>
+            <img
+              style={{ height: "200px" }}
+              src={beer.image_url}
+              alt="beer_image"
+            />
+            <h4>{beer.name}</h4>
+            <p>${beer.tagline}</p>
+            <p>Contributed By: {beer.contributed_by}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 
 export default AllBeersPage;
